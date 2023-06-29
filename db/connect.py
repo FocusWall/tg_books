f20be69a1a9872719032e083a9dee62d55326610
FocusWall <arzhakov99@hotmail.com>
2023-06-29 20:42:10 +0700

    Uploads book and adds it to the database

--- db/connect.py
+++ db/connect.py
@@ -1,41 +1,41 @@
 import psycopg2
 
-from misc import user, password, db, host
+from db.misc import user, password, db, host
 
 class Database:
-    def __init__(self, user=user, password=password, db=db, host=host):
-        self.user = user
-        self.password = password
-        self.db = db
-        self.host = host
+    user = user
+    password = password
+    db = db
+    host = host
 
     def connect(self):
         try:
-            conn = psycopg2.connect(dbname=self.db, user=self.user, password=self.password, host=self.host)
-            return conn
+            with psycopg2.connect(dbname=self.db, user=self.user, password=self.password, host=self.host) as conn:
+                return conn
         except:
             return None
-
+        
     @staticmethod
     def query_db(query, conn):
-        cursor = conn.cursor()
-        cursor.execute(query)
-        result = cursor.fetchall()
-        cursor.close()
-        conn.close()
-        return result
+        with conn.cursor() as cursor:
+                cursor.execute(query)
+                result = cursor.fetchall()
+                conn.commit()
+                conn.close()
+                return result
+            
 
-db = Database(user, password, db, host)
+#db = Database(user, password, db, host)
 
-db_conn = db.connect()
-query = "SELECT * FROM users"
-if db_conn:
-    try:
-        result = db.query_db(query, db_conn)
-        print(result)
-    except Exception as _er:
-        print("Incorrect query: ", _er)
-else:
-    print("Error has ocurred while connecting to database")
-
-
+#db_conn = db.connect()
+#query = "INSERT INTO users (id, name) VALUES (9, 'stacy') RETURNING id"
+#if db_conn:
+#    try:
+#        result = db.query_db(query, db_conn)
+#        print(result)
+#    except:
+#        print("Incorrect query")
+        
+    
+#else:
+#    print("Error has ocurred while connecting to database")
--- handlers/Users/book.py
+++ handlers/Users/book.py
@@ -3,8 +3,24 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.builtin import Command
 
 from loader import dp, bot
+from db.connect import Database
 from states.upload import Upload
 import uuid
+
+def upload_book_db(book_id, book_name, location):
+    db = Database()
+    db_conn = db.connect()
+    query = f"INSERT INTO books (id, book_name, location) VALUES ({book_id}, '{book_name}', '{location}') RETURNING id"
+    if db_conn:
+        try:
+            return db.query_db(query, db_conn)
+        except:
+            print(f"Incorrect query: {query}")
+            return None
+    else:
+        print("Can't connect to database")
+        return None
+            
 
 @dp.message_handler(Command("upload"))
 async def start_upload_file(message: types.Message):
@@ -13,16 +29,23 @@ async def start_upload_file(message: types.Message):
 
 @dp.message_handler(state=Upload.upload, content_types=['any'])
 async def upload_file(message: types.Message, state: FSMContext):
-    if message.document:
+    if message.document: # Checks if the file user dropped is actually a document
         file_id = message.document.file_id
-        file_name = message.document.file_name
+        book_name = message.document.file_name
         book_id = uuid.uuid4().fields[-1]
-
-        await message.answer("Worked")
+        location = f"/home/focus/project2/books/{book_id}"
         try:
-            await bot.download_file_by_id(file_id, f"/home/focus/project2/files/{file_name}")
+            await bot.download_file_by_id(file_id, location) # Downloads a file from telegram
         except:
             await message.answer("Error has ocurred while uploading file")
+        if upload_book_db(book_id, book_name, location): # Adds book to db
+                await message.answer("The book has successfuly uploaded")
+        else:
+            await message.answer("The book was not added to database")
     else:
         await message.answer("This is not a book! Try /upload again")
-    await state.finish()
+    await state.finish() # Unsets the state Upload in any case
+    
+    
+
+    
